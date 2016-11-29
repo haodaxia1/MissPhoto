54eb7d02eaf8ee9e572c64c9fa42e55aed34c207
Zy0214 <710035886@qq.com>
2016-11-29 09:31:42 +0800

    修改首页部分代码

--- app/src/main/java/com/example/administrator/missphoto/HomeFragment.java
+++ app/src/main/java/com/example/administrator/missphoto/HomeFragment.java
@@ -12,36 +12,33 @@ import android.widget.ViewFlipper;
 
 public class HomeFragment extends Fragment {
     private ViewFlipper flipper;
-    private int[] resId = {R.drawable.home_scr1,R.drawable.home_scr2,R.drawable.home_scr3};
+   /* private int[] resId = {R.drawable.home_scr1,R.drawable.home_scr2,R.drawable.home_scr3};*/
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.layout_home, container, false);
+    }
+      /*  flipper = (ViewFlipper)view.findViewById(R.id.flipper);
 
-
-        flipper = (ViewFlipper)View.findViewById(R.id.flipper);
-
-        /*
+        *//*
         * 动态导入的方式为ViewFlipper加入子View
-        * */
+        * *//*
         for (int i = 0; i < resId.length; i++) {
             flipper.addView(getImageView(resId[i]));
         }
-        /*
+        *//*
         * 为ViewFlipper去添加动画效果
-        * */
+        * *//*
         flipper.setInAnimation(this.getContext(), R.anim.left_in);
         flipper.setOutAnimation(this.getContext(), R.anim.left_out);
-
         flipper.setFlipInterval(2000);
         flipper.startFlipping();
     }
     private ImageView getImageView(int resId){
-        /*ImageView image = new ImageView(this);*/
+        *//*ImageView image = new ImageView(this);*//*
         ImageView image = new HomeFragment().getImageView(resId);
-        /*image.setBackgroundResource(resId);*/
+        image.setBackgroundResource(resId);
         return image;
-    }
-    }
-
+    }*/
 }
+
