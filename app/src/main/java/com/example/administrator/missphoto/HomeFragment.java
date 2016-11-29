0f8cdc44428de655702f0687a23e25f2358ae9b5
Zy0214 <710035886@qq.com>
2016-11-29 10:56:27 +0800

    增加首页轮播图代码

--- app/src/main/java/com/example/administrator/missphoto/HomeFragment.java
+++ app/src/main/java/com/example/administrator/missphoto/HomeFragment.java
@@ -6,42 +6,51 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
 
 public class HomeFragment extends Fragment {
     private ViewFlipper flipper;
-//    private int[] resId = {R.drawable.home_scr1,R.drawable.home_scr2,R.drawable.home_scr3};
+    private int[] resId = {R.drawable.yuepai, R.drawable.yuehua, R.drawable.shili1};
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.layout_home, container, false);
-
-//
-//        flipper = (ViewFlipper)View.findViewById(R.id.flipper);
-//
-//        /*
-//        * 动态导入的方式为ViewFlipper加入子View
-//        * */
-//        for (int i = 0; i < resId.length; i++) {
-//            flipper.addView(getImageView(resId[i]));
-//        }
-//        /*
-//        * 为ViewFlipper去添加动画效果
-//        * */
-//        flipper.setInAnimation(this.getContext(), R.anim.left_in);
-//        flipper.setOutAnimation(this.getContext(), R.anim.left_out);
-//
-//        flipper.setFlipInterval(2000);
-//        flipper.startFlipping();
-//    }
-//    private ImageView getImageView(int resId){
-//        /*ImageView image = new ImageView(this);*/
-//        ImageView image = new HomeFragment().getImageView(resId);
-//        /*image.setBackgroundResource(resId);*/
-//        return image;
-//    }
+        View view = inflater.inflate(R.layout.layout_home, container, false);
+        flipper = (ViewFlipper)view.findViewById(R.id.flipper);
+        /*
+        * 动态导入的方式为ViewFlipper加入子View
+        * */
+        for (int i = 0; i < resId.length; i++) {
+            flipper.addView(getImageView(resId[i]));
+        }
+        /*
+        * 为ViewFlipper去添加动画效果
+        * */
+        TranslateAnimation rightInAnim = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        rightInAnim.setDuration(1000);
+        TranslateAnimation leftOutAnim = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        leftOutAnim.setDuration(1000);
+        flipper.setInAnimation(rightInAnim);
+        flipper.setOutAnimation(leftOutAnim);
+        flipper.setFlipInterval(2000);
+        flipper.startFlipping();
+        return view;
     }
-
+        private ImageView getImageView(int resId) {
+            ImageView image = new ImageView(getActivity());
+        /*    ImageView image = new HomeFragment().getImageView(resId);*/
+            image.setBackgroundResource(resId);
+            return image;
+        }
 }
+
+
