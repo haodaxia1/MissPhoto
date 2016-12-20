32125cdb4ae99f3935889318808279369eb1a963
haodaxia1 <549787212@qq.com>
2016-12-20 19:21:12 +0800

    实现了与数据库交互登录的功能

--- app/src/main/java/com/example/administrator/missphoto/ZhuceActivity.java
+++ app/src/main/java/com/example/administrator/missphoto/ZhuceActivity.java
@@ -3,6 +3,8 @@ package com.example.administrator.missphoto;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -33,8 +35,22 @@ public class ZhuceActivity extends Activity {
     private EditText userName;
     private EditText passWord;
     private EditText uaccount;
+    private Button btn_zhuce_entry;
 
-
+    Handler handler2 = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+            if (msg.what == 0x123) {
+                Toast.makeText(ZhuceActivity.this, "登陆成功", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                intent.setClass(ZhuceActivity.this,GettimeActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(ZhuceActivity.this, "登陆失败", Toast.LENGTH_LONG).show();
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SMSSDK.initSDK(this, "191991a25bce4", "93d28288195be0f357eaf4f9e7ad87e4");
@@ -53,55 +69,50 @@ public class ZhuceActivity extends Activity {
             }
         });
         btn_ok=(Button)findViewById(R.id.btn_ok);
-//        btn_ok.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                Toast.makeText(ZhuceActivity.this,"success",Toast.LENGTH_SHORT).show();
-//                new Thread()
-//                {
-//                    public void run()
-//                    {
-//                        try
-//                        {
-//
-//                    urlPath = "http://172.16.22.152:8080/user/?obj=0&upwd="+passWord.getText().toString()
-//                            +"&uname="+URLEncoder.encode(userName.getText().toString(),"UTF-8")
-//                            +"&uaccount="+URLEncoder.encode(uaccount.getText().toString(),"UTF-8");
-//
-//                    url = new URL(urlPath);
-//
-//
-//                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//
-//                            if (conn.getResponseCode() == 200) {
-//                                // 获得服务器响应的数据
-//                                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-//                                // 数据
-//                                String retData = null;
-//                                String responseData = "";
-//                                while ((retData = in.readLine()) != null) {
-//                                    responseData += retData;
-//                                }
-//                                in.close();
-//                        }
-//                        } catch (Exception e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }.start();
-//            }
-//        });
-//        private void Login(){
-//            try {
-//                urlPath2 = "http://192.168.1.103:8992/user/?obj=1&upwd="+passWord.getText().toString()
-//                        +"&uname="+URLEncoder.encode(userName.getText().toString(),"UTF-8");
-//            } catch (UnsupportedEncodingException e) {
-//                e.printStackTrace();
-//            }
-//            login.sleep(1000);
-//        }
-//短信验证
+        btn_zhuce_entry=(Button)findViewById(R.id.btn_zhuce_entry);
+
+        btn_zhuce_entry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    urlPath2 = "http://172.16.17.133:8080/user/?obj=1&upwd="+passWord.getText().toString()
+                            +"&uname="+URLEncoder.encode(userName.getText().toString(),"UTF-8");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+                new Thread() {
+                    public void run ()
+                    {
+                        try {
+                            url = new URL(urlPath2);
+                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+                            if (conn.getResponseCode() == 200) {
+
+                                // 获得服务器响应的数据
+                                BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+                                // 数据
+                                String retData = null;
+                                String responseData = "";
+                                while ((retData = in.readLine()) != null) {
+                                    responseData += retData;
+                                }
+
+                                if (responseData.equals("ture")) {
+                                    handler2.sendEmptyMessage(0x123);
+                                }else {
+                                    handler2.sendEmptyMessage(0x122);
+                                }
+                                in.close();
+                            }
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }.start();
+            }
+        });
         btn_ok.setOnClickListener(new  View.OnClickListener(){
 
             @Override
@@ -126,7 +137,7 @@ public class ZhuceActivity extends Activity {
                                         try
                                         {
 
-                                            urlPath = "http://172.16.22.152:8080/user/?obj=0&upwd="+passWord.getText().toString()
+                                            urlPath = "http://172.16.17.133:8080/user/?obj=0&upwd="+passWord.getText().toString()
                                                     +"&uname="+URLEncoder.encode(userName.getText().toString(),"UTF-8")
                                                     +"&uaccount="+URLEncoder.encode(uaccount.getText().toString(),"UTF-8");
 
@@ -162,6 +173,7 @@ public class ZhuceActivity extends Activity {
                 registerPage.show(getBaseContext());
             }
         });
+
 
     }
 
